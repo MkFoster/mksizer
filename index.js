f2613675503c3ba92190ff80f3df916ce18f57d6
Mark Foster <mark@mkfoster.com>
2018-07-16 00:02:03 -0400

    Filtering on files with image extensions

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+"use strict";
+
+const util = require('util');
 const fs = require('fs');
+const path = require('path');
+const validImageExtensions = ['.png', '.jpg', '.jpeg', '.tiff', '.tif', '.gif', '.webp'];
 
 var validArgs = true;
 var sourceDir, destinationDir;
@@ -24,11 +29,34 @@ if (!validArgs) {
     process.exit();
 }
 
-function fsReadDirPromise() {
-
+/**
+ * Manually promisify fs.readdir vs. using util.promisify(fs.readdir)
+ * 
+ * @param {string} sourceDir 
+ */
+function fsReadDirPromise(sourceDir) {
+    return new Promise(function(resolve, reject) {
+        fs.readdir(sourceDir, (err, listing) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(listing);
+            }
+        });
+    });
 }
 
-fs.readdir(sourceDir, (err, listing) => {
+function filterImageExtensions(fileName) {
+    return validImageExtensions.includes(path.extname(fileName).toLowerCase());
+}
+
+const readdir = util.promisify(fs.readdir);
+
+readdir(sourceDir).then((listing) => {
     console.log(listing);
+    var imageList = listing.filter(filterImageExtensions);
+    
+}).catch((err) => {
+    console.log(err)
 });
 
